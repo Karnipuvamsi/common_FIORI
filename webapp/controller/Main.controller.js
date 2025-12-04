42c8b7f1d0ee8f1d375aaca000c8eedc32698417
Karnipu Vamsi <karnipu.vamsi@genpact.com>
2025-12-04 06:58:03 +0000

    fiori fixes

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageBox",
-	"mathbasics/BasicMath"
+	"csc/BasicMath"
 ], function (Controller, MessageBox, BasicMath)  {
     "use strict";
 
@@ -15,16 +15,16 @@ sap.ui.define([
 		onTestMath: function () {
 			try {
 				// Use BasicMath from the deployed library
-				var sum = BasicMath.add(10,100);
+				var sum = BasicMath.add(10,1000);
 				var product = BasicMath.multiply(5, 3);
 				var difference = BasicMath.subtract(50, 10);
-				var quotient = BasicMath.divide(100, 5);
+				var quotient = BasicMath.divide(100, 50);
 
 				var message = "Math Operations Results:\n\n" +
-					"10 + 100 = " + sum + "\n" +
+					"10 + 1000 = " + sum + "\n" +
 					"5 × 3 = " + product + "\n" +
 					"50 - 10 = " + difference + "\n" +
-					"100 ÷ 5 = " + quotient;
+					"100 ÷ 50 = " + quotient;
 
 				MessageBox.show(message, {
 					title: "BasicMath Library Test",
